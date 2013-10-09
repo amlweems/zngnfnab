5d2ba82de91c92a0100df4be2d466b847994150e
Anthony Weems <amlweems@gmail.com>
2013-10-08 22:34:39 -0500

    switched to the 4 space convention

--- hexutils.py
+++ hexutils.py
@@ -2,27 +2,27 @@ import binascii
 import base64
 
 def htos(s):
-	return binascii.unhexlify(s)
+    return binascii.unhexlify(s)
 
 def stoh(s):
-	return binascii.hexlify(s)
+    return binascii.hexlify(s)
 
 def stob(s):
-	return base64.b64encode(s)
+    return base64.b64encode(s)
 
 def btos(s):
-	return base64.b64decode(s)
+    return base64.b64decode(s)
 
 def htob(s):
-	return stob(htos(s))
+    return stob(htos(s))
 
 def btoh(s):
-	return stoh(btos(s))
+    return stoh(btos(s))
 
 if __name__ == "__main__":
-	hex_str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-	base_str = htob(hex_str)
-	new_hex_str = btoh(base_str)
-	print hex_str
-	print base_str
-	print new_hex_str
+    hex_str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+    base_str = htob(hex_str)
+    new_hex_str = btoh(base_str)
+    print hex_str
+    print base_str
+    print new_hex_str
