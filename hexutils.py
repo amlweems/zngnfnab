cde93dc5620a23daf4819846531d14a361bc48d7
Anthony Weems <amlweems@gmail.com>
2013-10-09 17:35:47 -0500

    added more descriptive names and output, bounds checking on key

--- hexutils.py
+++ hexutils.py
@@ -20,9 +20,10 @@ def btoh(s):
     return stoh(btos(s))
 
 if __name__ == "__main__":
+    print "## Program 1 ##"
     hex_str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
     base_str = htob(hex_str)
     new_hex_str = btoh(base_str)
-    print hex_str
-    print base_str
-    print new_hex_str
+    print "Hex:          {}".format(hex_str)
+    print "Base64:       {}".format(base_str)
+    print "Hex'd Base64: {}".format(new_hex_str)
--- xor.py
+++ xor.py
@@ -1,12 +1,14 @@
 from hexutils import *
 
-def xor(p, k):
-    return ''.join(chr(ord(p[i])^ord(k[i])) for i in range(len(p)))
+def xor(plain, key):
+    return ''.join(chr(ord(plain[i])^ord(key[i%len(key)])) for i in range(len(plain)))
 
 if __name__ == "__main__":
+    # Program 2 Tests
+    print "## Program 2 ##"
     plain = htos("1c0111001f010100061a024b53535009181c")
     key = htos("686974207468652062756c6c277320657965")
     enc = xor(plain, key)
-    print stoh(plain)
-    print stoh(key)
-    print stoh(enc)
+    print "Plain:     {}".format(stoh(plain))
+    print "Key:       {}".format(stoh(key))
+    print "Encrypted: {}".format(stoh(enc))
