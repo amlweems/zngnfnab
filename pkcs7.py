5a71e9ac3afa0b11a258c881caf8d0f5fea45121
Anthony Weems <amlweems@gmail.com>
2014-01-12 00:45:49 -0600

    completed problem 9: implementing pkcs#7

---
+++ pkcs7.py
@@ -0,0 +1,13 @@
+
+def encode(text, block_size):
+    padding = block_size - len(m)%block_size
+    return text + chr(padding)*padding
+
+def decode(text):
+    padding = ord(text[-1])
+    if len(text) < padding:
+        return text
+    for i in range(2, padding+1):
+        if text[-i] != chr(padding):
+            return text
+    return text[:-padding]
