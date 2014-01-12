e41ddef0f47fd0cb5678f3abca8895cd95e38c0f
Anthony Weems <amlweems@gmail.com>
2014-01-12 01:15:14 -0600

    completed problem 10: implementing aes-128-cbc (relies on aes-128-ecb implementation)

--- aes.py
+++ aes.py
@@ -1,5 +1,6 @@
 from Crypto.Cipher import AES
 from hexutils import *
+from xor import xor
 import requests
 
 def dup_blocks(enc):
@@ -12,3 +13,25 @@ def dup_blocks(enc):
         else:
             blocks.add(block)
     return duplicates
+
+def cbc_encrypt(text, key, iv, block_size=16):
+    cipher = AES.new(key, AES.MODE_ECB)
+    enc = ''
+    prev = iv
+    for n in range(0, len(text), block_size):
+        block = xor(text[n:n+block_size], prev)
+        enc_block = cipher.encrypt(block)
+        enc += enc_block
+        prev = enc_block
+    return enc
+
+def cbc_decrypt(enc, key, iv, block_size=16):
+    cipher = AES.new(key, AES.MODE_ECB)
+    dec = ''
+    prev = iv
+    for n in range(0, len(enc), block_size):
+        block = enc[n:n+block_size]
+        dec_block = cipher.decrypt(block)
+        dec += xor(dec_block, prev)
+        prev = block
+    return dec
